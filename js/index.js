0686efc2deecbbe44c5bcf980a09df1d83d258eb
Colin Brogan <colinbrogan@gmail.com>
2015-08-19 23:42:11 -0500

    progress

--- js/index.js
+++ js/index.js
@@ -46,7 +46,10 @@ function buildScreen() {
     }).css("width",resolutionX*tileSize);
 }
 
+/*******    SHAPE OBJECT     ********/
+/*******                     ********/
 var Shape = {
+
   landed: false,
   currentShapeIndex: 0,
   tiles: [
@@ -67,6 +70,7 @@ var Shape = {
     // Eventually make this newShape.length > resolutionY
     var randomYOffset = 0;
 
+    // Convert Shape Strings into x,y coordinates for this.tiles
     for(var rowIndex = 0; rowIndex < newShape.length; rowIndex++) {
       var row = newShape[rowIndex];
       for(var columnIndex = 0; columnIndex < row.length; columnIndex++) {
@@ -79,25 +83,23 @@ var Shape = {
         }
       }
     }
-    console.log("Shapes[randomIndex]");
-    console.log(newShape);
-    console.log("Shape.tiles:");
-    console.log(this.tiles);
+
   },
   moveLeft: function() {
     console.log("Shape.moveLeft()");
     if(Platform.getsLandedOnBy(this.tiles)) {
-      Platform.freezeShape(this);
+      console.log("this.landed set to true");
+      this.landed = true;
+      return false;
     } else {
-      var tempStoreTiles = this.tiles;
+      var tempStoreTiles = this.tiles.splice(0);
       this.tiles = [];
       for(tile in tempStoreTiles) {
-        var tileX = tile[0];
-        var tileY = tile[1];
-        this.tiles.push([tileX - 1,tileY]);
+        var tileX = tempStoreTiles[tile][0];
+        var tileY = tempStoreTiles[tile][1];
+        Shape.tiles.push([tileX - 1,tileY]);
       }
-      console.log("new this.tiles:");
-      console.log(this.tiles);
+      return true;
     }
 
   },
@@ -109,10 +111,12 @@ var Shape = {
   },
   writeShades: function() {
     // clear prior shape shades to prepare for new shading
+    console.log("Shape.writeShades")
     $grid.find(".tile").removeClass("shape-shade");
-    for(tile in this.tile) {
-      var tileX = tile[0];
-      var tileY = tile[1];
+    for(tile in this.tiles) {
+      var tileX = this.tiles[tile][0];
+      var tileY = this.tiles[tile][1];
+      console.log($(makeCoordinates(tileX,tileY)));
       $(makeCoordinates(tileX,tileY)).addClass("shape-shade");
     }
   }
@@ -121,14 +125,36 @@ var Shape = {
 var Platform = {
   tiles: [
   ],
-  freezeShape: function(Shape) {
+  freezeShape: function(shapeInstance) {
     console.log("Platform.freezeShape");
+    for(var stI in shapeInstance.tiles) {
+      var shapeTileX = shapeInstance.tiles[stI][0];
+      var shapeTileY = shapeInstance.tiles[stI][1];
+      this.tiles.push([shapeTileX,shapeTileY]);
+    }
+    console.log("new Platform tiles");
+    console.log(this.tiles);
   },
   checkOverflow: function() {
     console.log("Platform.checkOverflow");
   },
-  getsLandedOnBy: function(tiles) {
+  getsLandedOnBy: function(shapeTiles) {
     console.log("Platform.getsLandedOnBy");
+    for(var stI in shapeTiles) {
+      var shapeTileX = shapeTiles[stI][0];
+      var shapeTileY = shapeTiles[stI][1];
+      if(shapeTileX == 1) {
+        return true;
+      }
+      for(var ptI in this.tiles) {
+        var platTileX = this.tiles[ptI][0];
+        var platTileY = this.tiles[ptI][1];
+        if(shapeTileX - platTileX <= 1 && shapeTileY == platTileY) {
+          alert("this happened");
+          return true;
+        }
+      }
+    }
     return false;
   },
   clearFullColumn: function() {
@@ -136,29 +162,41 @@ var Platform = {
   },
   writeShades: function() {
     console.log("Platform.writeShades");
+    $grid.find(".tile").removeClass("plat-shade");
+    $grid.find(".tile").removeClass("shape-shade");
+    for(tile in this.tiles) {
+      var tileX = this.tiles[tile][0];
+      console.log(tileX);
+      var tileY = this.tiles[tile][1];
+      console.log(tileY);
+      console.log($(makeCoordinates(tileX,tileY)));
+      $(makeCoordinates(tileX,tileY)).addClass("plat-shade");
+    }
   },
 }
+var firedOnce = false;
 
 var frameEvent = function() {
-  if(Shape.landed) {
+  if(Shape.moveLeft()) {
+    Shape.writeShades();
+  } else if(!firedOnce) {
+    firedOnce = true;
     Platform.freezeShape(Shape);
     Shape.generateAnew();
     Platform.writeShades();
-  } else { 
-    Shape.moveLeft();
   }
-  Shape.writeShades();
 }
 
 
 $(document).ready(function() {
   buildScreen();
   Shape.generateAnew();
-//  frameEvent();
-//  setInterval(frameEvent, 1000/fps);
-  $grid.unbind("keydown")
-  $grid.on("keydown",function(event) { 
+  frameEvent();
+  setInterval(frameEvent, 1000/fps);
+  $(window).unbind("keydown");
+  $(window).on("keydown",function(event) { 
     if(event.keyCode == 37) {
+      console.log("hit left key");
       frameEvent();
     } else if (event.keyCode == 38) {
       Shape.moveUp();
